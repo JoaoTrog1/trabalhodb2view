ae3ffd32853ca8b517f9833d3219c5100abd730c
JoaoTrog1 <146787656+JoaoTrog1@users.noreply.github.com>
2024-08-27 22:04:44 -0300

    Update exemplosView.sql
    primeiro exemplo de view

--- exemplosView.sql
+++ exemplosView.sql
@@ -1,1 +1,5 @@
 
+/* retorna todos os pedidos */
+CREATE VIEW vw_pedidos AS
+SELECT C.nome_cliente, PD.nome_produto, P.data_pedido FROM tbpedido P INNER JOIN tbproduto PD ON P.id_produto = PD.id_produto INNER JOIN tbcliente C ON P.id_cliente = C.id_cliente;
+
